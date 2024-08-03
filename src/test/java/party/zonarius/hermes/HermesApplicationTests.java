2326a2693e025382242c54f8dfe45caccd4e79fe
Zonarius <zonarius@live.at>
2024-08-03 11:21:31 +0200

    Initial commit

---
+++ src/test/java/party/zonarius/hermes/HermesApplicationTests.java
@@ -0,0 +1,13 @@
+package party.zonarius.hermes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
